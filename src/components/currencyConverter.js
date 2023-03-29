92c28478cac76be051082fa6a2d9651e3f576787
Martin <huim13a@gmail.com>
2023-03-29 15:57:26 +0800

    update converter

--- src/components/currencyConverter.js
+++ src/components/currencyConverter.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import { motion } from "framer-motion";
 import Logo from "../images/metaphorLab.png"
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 
 export default function CurrencyConverter(){
     const diaLogDivStyle = {
         width: "400px",
-        height: "300px"
+        height: "450px"
+    }
+    const iconStyle = {
+        color: "#f30987",
+        width: "30px",
+        height: "30px",
+        padding: "15px 15px"
     }
     const textfieldSXStyle = {
         width: "30%",
@@ -83,9 +91,11 @@ export default function CurrencyConverter(){
         const inputValue = e.target.value
         if (inputValue !== ""){
             setCustomPriceData(parseFloat(inputValue).toFixed(2)*priceData)
+        } else {
+            setCustomPriceData(0)
         }
     }
-    const [customPriceData, setCustomPriceData] = useState([])
+    const [customPriceData, setCustomPriceData] = useState(0)
     const [priceData, setPriceData] = useState([])
     const fetchPriceData = () => {
         fetch("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=HKD")
@@ -123,10 +133,29 @@ export default function CurrencyConverter(){
                                 (between HKD & ETH)
                             </p>
                             <br/>
+                            <div>
+                                <motion.svg
+                                    style={iconStyle}
+                                    animate={{
+                                        scale: [1, 1.5, 1.5, 1.5, 1],
+                                        rotate: [0, 0, 180, 360, 360]
+                                    }}
+                                    transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        times: [0, 0.2, 0.5, 0.8, 1],
+                                        repeat: Infinity,
+                                        repeatDelay: 0.5
+                                    }}
+                                >
+                                    <CurrencyExchangeIcon/>
+                                </motion.svg>
+                            </div>
                             <p style={pinkDialogText}>
                                 1 ETH worths &nbsp;<h4 style={{margin:0}}>${priceData}</h4>&nbsp; HKD currently
                             </p>
                         </div>
+                        <br/><br/>
                         <div style={converterDivStyle}>
                             <TextField
                                 sx={textfieldSXStyle}
